201f13491f53a4966d266333cb9e8db18582fb16
JD <62799697+Jd5221@users.noreply.github.com>
2021-02-25 20:36:40 -0500

    Add files via upload

---
+++ runme.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+loc=/usr/bin/
+
+if [ ! -e "$loc/base32" ] ; then
+	echo "you are missing base32 from your $loc directory"
+else
+	echo 
+
+fi
+########################
+if [ ! -e "$loc/base64" ] ; then
+	echo "you are missing base64 from your $loc directory"
+else
+	echo 
+fi
+########################
+if [ ! -e "$loc/perl" ] ; then
+	echo "you are missing perl from your $loc directory"
+else
+	echo 
+fi
+########################
+sudo chmod +x def
+sudo chmod 777 def
+sudo chmod +x numbers
+sudo chmod 777 numbers
+sudo cp -i def /usr/bin 
+
